44c151d5170e5b96dbeb8f8b43681abcd56edb2b
lrnzznn1 <lorenzo.zanini@studenti.unitn.it>
2024-06-28 14:27:19 +0200

    First Commit

--- app/src/main/java/com/lrnzznn/pokedexrawfish/MainActivity.kt
+++ app/src/main/java/com/lrnzznn/pokedexrawfish/MainActivity.kt
@@ -3,14 +3,19 @@ package com.lrnzznn.pokedexrawfish
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lrnzznn.pokedexrawfish.ui.theme.PokedexRawFishTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +27,60 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Main()
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Main(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .verticalScroll(rememberScrollState())
+        ) {
+            for (i in 1..15) {
+                Pokemon(modifier = modifier, i = i)
+            }
+        }
+    }
 }
+
+
+
+@Composable
+fun Pokemon(modifier: Modifier, i : Int){
+    Box(
+        modifier = Modifier
+            .padding(top = 40.dp, bottom = 40.dp)
+    ){
+        Column{
+            BasicText(
+                text = "prova $i",
+                modifier = modifier
+            )
+            BasicText(
+                text = "descrizione di $i",
+                modifier = modifier
+            )
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PokedexRawFishTheme {
-        Greeting("Android")
+        Main()
     }
 }
